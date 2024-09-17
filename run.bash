5c0f124735df9795044f19b6842dc26b828dcdda
mint44 <minhbui0224@outlook.com>
2024-09-17 14:16:11 -0400

    git commit -m "Replace old code with new local code"

---
+++ run.bash
@@ -0,0 +1,19 @@
+# python src/reconstruct_ds1.py --index 0 --batch_min_dur 1000 --batch_max_dur 2000
+python src/reconstruct_ds1.py --index 0  --batch_min_dur 0 --batch_max_dur 1000 
+# python src/reconstruct_ds1.py --index 1 --batch_min_dur 1000 --batch_max_dur 2000
+python src/reconstruct_ds1.py --index 2 --batch_min_dur 1000 --batch_max_dur 2000
+python src/reconstruct_ds1.py --index 3 --batch_min_dur 1000 --batch_max_dur 2000
+python src/reconstruct_ds1.py --index 4 --batch_min_dur 1000 --batch_max_dur 2000
+
+python src/reconstruct_ds1.py --index 0 --batch_min_dur 2000 --batch_max_dur 3000
+python src/reconstruct_ds1.py --index 1 --batch_min_dur 2000 --batch_max_dur 3000
+python src/reconstruct_ds1.py --index 2 --batch_min_dur 2000 --batch_max_dur 3000
+python src/reconstruct_ds1.py --index 3 --batch_min_dur 2000 --batch_max_dur 3000
+python src/reconstruct_ds1.py --index 4 --batch_min_dur 2000 --batch_max_dur 3000
+
+python src/reconstruct_ds1.py --index 0 --batch_min_dur 3000 --batch_max_dur 4000
+python src/reconstruct_ds1.py --index 1 --batch_min_dur 3000 --batch_max_dur 4000
+python src/reconstruct_ds1.py --index 2 --batch_min_dur 3000 --batch_max_dur 4000
+python src/reconstruct_ds1.py --index 3 --batch_min_dur 3000 --batch_max_dur 4000
+python src/reconstruct_ds1.py --index 4 --batch_min_dur 3000 --batch_max_dur 4000
+
